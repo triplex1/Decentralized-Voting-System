3ef5fb38ffd590776fc28fb8c31c6f4ed035f453
triplex1 <jrtagalogon@gmail.com>
2026-02-03 00:36:04 +0800

    Added Access Control (Admin/Owner) and overrides protection

--- votingSystem.sol
+++ votingSystem.sol
@@ -1,26 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.33;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract VotingSystem is Ownable {
+    constructor() Ownable(msg.sender) {}  // <-- Fix added here 
 
-contract votingSystem{
     struct Candidate {
         string name;
         uint256 voteCount;
     }
-    mapping (address => bool) public hasVoted;
+
+    mapping(address => bool) public hasVoted;
+    mapping(string => bool) private _candidateNames;
     Candidate[] public candidates;
 
-    //add candidate
-    function addCandidate(string memory _name) public {
+    event CandidateAdded(string name, uint256 index);
+    event Voted(address voter, uint256 candidateIndex);
+
+    function addCandidate(string memory _name) public onlyOwner {
+        require(!_candidateNames[_name], "Candidate already exists");
+        _candidateNames[_name] = true;
         candidates.push(Candidate(_name, 0));
+        emit CandidateAdded(_name, candidates.length - 1);
     }
 
-    //vote function
-    function vote(uint256 _candidateIndex) public {
-        require(!hasVoted[msg.sender], "You have already voted");
-
-        candidates[_candidateIndex].voteCount += 1;
+    function vote(uint256 _candidateIndex) public virtual {
+        require(!hasVoted[msg.sender], "Already voted");
+        require(_candidateIndex < candidates.length, "Invalid candidate");
+        candidates[_candidateIndex].voteCount++;
         hasVoted[msg.sender] = true;
+        emit Voted(msg.sender, _candidateIndex);
     }
 
+    function getCandidateCount() public view returns (uint256) {
+        return candidates.length;
+    }
 }
